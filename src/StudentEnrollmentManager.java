2695478203079903a7f5c9612029e657bba7aaba
Kayden Giang <nhatkhanhgiang2512@gmail.com>
2022-03-16 22:17:36 +0700

    defined more methods for the StudentEnrollmentManager interface

--- src/StudentEnrollmentManager.java
+++ src/StudentEnrollmentManager.java
@@ -1,6 +1,20 @@
 import java.util.List;
 
 public interface StudentEnrollmentManager {
+    void addNewStudent(Student student);
+
+    void addNewCourse(Course course);
+
+    void addNewEnrollment(String sid, String cid, String semester);
+
+    void deleteCurrentEnrollment(String sid, String cid, String semester);
+
+    Student getStudent(String sid);
+
+    Course getCourse(String cid);
+
+    StudentEnrollment getEnrollment(String sid, String cid, String semester);
+
     List<Student> getAllStudents();
 
     List<Course> getAllCourses();
